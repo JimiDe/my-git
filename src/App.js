615841b61901058de30e0ffb947e87b0cfcd6a9b
baiymin1213 <bai.yanmin2@iwhalecloud.com>
2020-07-23 23:03:05 +0800

    src text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit hahaha<code>src/App.js</code> and save to reload hahahahahahaha.
+          Edit hahaha<code>src/App.js hahaha</code> and save to reload hahahahahahaha.
         </p>
         <a
           className="App-link"
